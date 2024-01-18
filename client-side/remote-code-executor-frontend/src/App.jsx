36555c474c88daa55c3830169b565211dda13096
chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>
2024-01-18 22:01:31 +0530

    Made the first page

--- client-side/remote-code-executor-frontend/src/App.jsx
+++ client-side/remote-code-executor-frontend/src/App.jsx
@@ -1,18 +1,26 @@
-import Button from '@mui/material/Button';
-import CodeMirror from "@uiw/react-codemirror";
+import { useState } from 'react';
+import CodeEditor from './components/CodeEditor';
+import { Box } from '@mui/material';
+import Header from "./components/Header";
+import { MyContext } from './context/Context';
+import { javascript } from '@codemirror/lang-javascript';
+import Output from './components/Output';
 
 function App() {
+  const [code, setCode] = useState(
+  `function add(a, b) {\n  return a + b;\n}`);
+
+  const [lang,setLang] = useState(javascript);
 
   return (
-    <div>
-      <Button variant="contained">Hello world</Button>
-      <CodeMirror
-      value={code}
-      height="100px"
-      theme={vscodeDark}
-    />
-    </div>
+    <Box sx={{backgroundColor : "#22222b" , height:"100%"}}>
+      <MyContext.Provider value={{code , setCode , lang , setLang}}>
+        <Header />
+        <CodeEditor />
+        <Output />
+      </MyContext.Provider>
+     </Box>
   )
 }
 
-export default App
+export default App;
---
+++ client-side/remote-code-executor-frontend/src/components/CodeEditor.jsx
@@ -0,0 +1,27 @@
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import CodeMirror from "@uiw/react-codemirror";
+import { Box } from "@mui/material";
+import { useContext } from "react";
+import { MyContext } from "../context/Context";
+
+function CodeEditor() {
+    const { code, setCode, lang } = useContext(MyContext);
+    
+    const typeCode = (value , valueUpdate) => {
+        setCode(value);
+    }
+
+    return (
+      <Box sx={{display :"flex" , justifyContent:"center" , marginTop :"25px"}}>
+        <CodeMirror
+          value={code}
+          height="60vh"
+          width="90vw"
+          theme={vscodeDark}
+          extensions={[lang]}
+          onChange={typeCode}
+        />
+      </Box>
+    );
+  }
+export default CodeEditor;
