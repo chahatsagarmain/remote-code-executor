25b783dcf48a94ada34f9d170d01970c70797d9b
chahat sagar <109112505+chahatsagarmain@users.noreply.github.com>
2024-02-03 01:08:18 +0530

    .

--- server-side/python-code-executor/python_executor/python_executor/settings.py
+++ server-side/python-code-executor/python_executor/python_executor/settings.py
@@ -137,4 +137,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:80']
+CORS_ALLOWED_ORIGINS = ['http://localhost:80',
+                        "http://ec2-43-204-212-9.ap-south-1.compute.amazonaws.com"]
